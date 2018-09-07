ef87107c4e3ca31ec06d8bdbe2c235a7f0b1afd6
GinoAntonel <ginoantonel.ga@gmail.com>
2018-09-07 11:52:04 -0300

    soy un pez

---
+++ config/compras/models.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+class Compras(models.Model):
+    texto = models.CharField('Texto', max_length=50, null=False, blank=False)
+    fecha = models.DateTimeField('Fecha', auto_now=True)
+    
+    def __str__(self):
+        return 'Compra: {}'.format(self.texto)
